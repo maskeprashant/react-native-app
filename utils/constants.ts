769933b887b4efd202ae5c8a1932926545b55d5f
Prashant <maskeprashant.07@gmail.com>
2026-02-06 23:52:12 +0530

    add constants file for icon imports

---
+++ utils/constants.ts
@@ -0,0 +1,9 @@
+import home from "@/assets/icons/home.png";
+import list from "@/assets/icons/list.png";
+import reports from "@/assets/icons/reports.png";
+
+export const ICONS = {
+  home,
+  list,
+  reports,
+};
