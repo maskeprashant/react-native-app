f5f1763b040de405662ad83e57148e0cb97732bb
Prashant <maskeprashant.07@gmail.com>
2026-02-08 22:44:53 +0530

    feat: add background image constant to IMAGES export in constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,13 @@
 import home from "@/assets/icons/home.png";
 import list from "@/assets/icons/list.png";
 import reports from "@/assets/icons/reports.png";
-
+import backgroundImage from "@/assets/images/bg.jpg";
 export const ICONS = {
   home,
   list,
   reports,
+};
+
+export const IMAGES = {
+  backgroundImage,
 };
